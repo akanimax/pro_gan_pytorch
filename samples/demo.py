193958df7c3f54372a9409071aebed9f52ab2570
Animesh Karnewar <akanimax@gmail.com>
2019-01-10 14:23:19 +0530

    pip'ing fixed changes

--- samples/demo.py
+++ samples/demo.py
@@ -8,7 +8,7 @@ from pro_gan_pytorch import PRO_GAN as pg
 # ==========================================================================
 depth = 8
 num_points = 10
-transition_points = 90
+transition_points = 5
 # ==========================================================================
 
 # create the device for running the demo:
@@ -18,11 +18,13 @@ device = th.device("cuda" if th.cuda.is_available() else "cpu")
 gen = th.nn.DataParallel(pg.Generator(depth=9))
 gen.load_state_dict(th.load("GAN_GEN_SHADOW_8.pth", map_location=str(device)))
 
+
 # function to generate an image given a latent_point
 def get_image(point):
     img = gen(point, depth=depth, alpha=1).detach().squeeze(0).permute(1, 2, 0)
     img = (img - img.min()) / (img.max() - img.min())
     return img.cpu().numpy()
+
 
 # generate the set of points:
 fixed_points = th.randn(num_points, 512).to(device)
@@ -44,13 +46,16 @@ fig, ax = plt.subplots()
 plt.axis("off")
 shower = plt.imshow(get_image(start_point))
 
+
 def init():
     return shower,
+
 
 def update(point):
     shower.set_data(get_image(point))
     return shower,
 
+
 ani = FuncAnimation(fig, update, frames=points,
-                    init_func=init, blit=False	)
+                    init_func=init, blit=False)
 plt.show()
