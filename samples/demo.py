68dab2a79a136a1b90181d44300c22b8b6145f52
animesh kanrewar <akanimax@gmail.com>
2019-04-28 20:35:09 +0530

    latest stuff added

--- samples/demo.py
+++ samples/demo.py
@@ -1,63 +1,124 @@
+""" live (realtime) latent space interpolations of trained models """
+
+import argparse
 import torch as th
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from pro_gan_pytorch import PRO_GAN as pg
-
-# ==========================================================================
-# Tweakable parameters
-# ==========================================================================
-depth = 8
-num_points = 12
-transition_points = 30
-# ==========================================================================
+from pro_gan_pytorch.PRO_GAN import Generator
+from torchvision.utils import make_grid
+from math import ceil, sqrt
+from scipy.ndimage import gaussian_filter
 
 # create the device for running the demo:
 device = th.device("cuda" if th.cuda.is_available() else "cpu")
 
-# load the model for the demo
-gen = th.nn.DataParallel(pg.Generator(depth=9))
-gen.load_state_dict(th.load("GAN_GEN_SHADOW_8.pth", map_location=str(device)))
+
+def parse_arguments():
+    """
+    command line arguments parser
+    :return: args => parsed command line arguments
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--generator_file", action="store", type=str,
+                        default=None, help="path to the trained generator model")
+
+    parser.add_argument("--depth", action="store", type=int,
+                        default=9, help="Depth of the network")
+
+    parser.add_argument("--latent_size", action="store", type=int,
+                        default=512, help="Latent size for the network")
+
+    parser.add_argument("--num_points", action="store", type=int,
+                        default=12, help="Number of samples to be seen")
+
+    parser.add_argument("--transition_points", action="store", type=int,
+                        default=30,
+                        help="Number of transition samples for interpolation")
+
+    parser.add_argument("--smoothing", action="store", type=float,
+                        default=1.0,
+                        help="amount of transitional smoothing")
+
+    args = parser.parse_args()
+
+    return args
 
 
-# function to generate an image given a latent_point
-def get_image(point):
-    img = gen(point, depth=depth, alpha=1).detach().squeeze(0).permute(1, 2, 0)
-    img = (img - img.min()) / (img.max() - img.min())
-    return img.cpu().numpy()
+def adjust_dynamic_range(data, drange_in=(-1, 1), drange_out=(0, 1)):
+    """
+    adjust the dynamic colour range of the given input data
+    :param data: input image data
+    :param drange_in: original range of input
+    :param drange_out: required range of output
+    :return: img => colour range adjusted images
+    """
+    if drange_in != drange_out:
+        scale = (np.float32(drange_out[1]) - np.float32(drange_out[0])) / (
+                np.float32(drange_in[1]) - np.float32(drange_in[0]))
+        bias = (np.float32(drange_out[0]) - np.float32(drange_in[0]) * scale)
+        data = data * scale + bias
+    return th.clamp(data, min=0, max=1)
 
 
-# generate the set of points:
-fixed_points = th.randn(num_points, 512).to(device)
-fixed_points = (fixed_points / fixed_points.norm(dim=1, keepdim=True)) * (512 ** 0.5)
-points = []  # start with an empty list
-for i in range(len(fixed_points) - 1):
-    pt_1 = fixed_points[i].view(1, -1)
-    pt_2 = fixed_points[i + 1].view(1, -1)
-    direction = pt_2 - pt_1
-    for j in range(transition_points):
-        pt = pt_1 + ((direction / transition_points) * j)
-        pt = (pt / pt.norm()) * (512 ** 0.5)
-        points.append(pt)
-    # also append the final point:
-    points.append(pt_2)
-
-start_point = points[0]
-points = points[1:]
-
-fig, ax = plt.subplots()
-plt.axis("off")
-shower = plt.imshow(get_image(start_point))
+def get_image(gen, point, depth, alpha):
+    """
+    obtain an All-resolution grid of images from the given point
+    :param gen: the generator object
+    :param point: random latent point for generation
+    :param depth: value of depth for image generation (0 indexed)
+    :param alpha: value of alpha for fade-in (between 0 and 1)
+    :return: img => generated image
+    """
+    image = gen(point, depth, alpha).detach()
+    image = adjust_dynamic_range(image).squeeze(dim=0)
+    return image.cpu().numpy().transpose(1, 2, 0)
 
 
-def init():
-    return shower,
+def main(args):
+    """
+    Main function for the script
+    :param args: parsed command line arguments
+    :return: None
+    """
+
+    # load the model for the demo
+    gen = th.nn.DataParallel(
+        Generator(
+            depth=args.depth,
+            latent_size=args.latent_size))
+    gen.load_state_dict(th.load(args.generator_file, map_location=str(device)))
+
+    # generate the set of points:
+    total_frames = args.num_points * args.transition_points
+    all_latents = th.randn(total_frames, args.latent_size).to(device)
+    all_latents = th.from_numpy(
+        gaussian_filter(
+            all_latents.cpu(),
+            [args.smoothing * args.transition_points, 0], mode="wrap"))
+    all_latents = (all_latents /
+                   all_latents.norm(dim=-1, keepdim=True)) * sqrt(args.latent_size)
+
+    start_point = th.unsqueeze(all_latents[0], dim=0)
+    points = all_latents[1:]
+
+    fig, ax = plt.subplots()
+    plt.axis("off")
+    shower = plt.imshow(get_image(gen, start_point, args.depth - 1, 1))
+
+    def init():
+        return shower,
+
+    def update(point):
+        shower.set_data(get_image(gen, th.unsqueeze(point, dim=0), args.depth - 1, 1))
+        return shower,
+
+    # define the animation function
+    ani = FuncAnimation(fig, update, frames=points, 
+                        init_func=init)
+    plt.show(ani)
 
 
-def update(point):
-    shower.set_data(get_image(point))
-    return shower,
-
-
-ani = FuncAnimation(fig, update, frames=points,
-                    init_func=init, blit=False)
-plt.show()
+if __name__ == '__main__':
+    main(parse_arguments())
---
+++ samples/latent_space_interpolation.py
@@ -0,0 +1,123 @@
+""" script for generating samples from a trained model """
+
+import torch as th
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import os
+from torch.backends import cudnn
+from torchvision.utils import make_grid
+from math import sqrt, ceil
+from tqdm import tqdm
+from scipy.ndimage import gaussian_filter
+
+# turn fast mode on
+cudnn.benchmark = True
+
+# define the device for the training script
+device = th.device("cuda" if th.cuda.is_available() else "cpu")
+
+
+def parse_arguments():
+    """
+    command line arguments parser
+    :return: args => parsed command line arguments
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--generator_file", action="store", type=str,
+                        help="pretrained weights file for generator", required=True)
+
+    parser.add_argument("--latent_size", action="store", type=int,
+                        default=512,
+                        help="latent size for the generator")
+
+    parser.add_argument("--depth", action="store", type=int,
+                        default=9,
+                        help="latent size for the generator")
+
+    parser.add_argument("--time", action="store", type=float,
+                        default=300,
+                        help="Number of seconds for the video to make")
+
+    parser.add_argument("--fps", action="store", type=int,
+                        default=60, help="Frames per second in the video")
+
+    parser.add_argument("--smoothing", action="store", type=float,
+                        default=0.75, help="Smoothing amount in transition frames")
+
+    parser.add_argument("--out_dir", action="store", type=str,
+                        default="interp_animation_frames/",
+                        help="path to the output directory for the frames")
+
+    args = parser.parse_args()
+
+    return args
+
+def adjust_dynamic_range(data, drange_in=(-1, 1), drange_out=(0, 1)):
+    if drange_in != drange_out:
+        scale = (np.float32(drange_out[1]) - np.float32(drange_out[0])) / (np.float32(drange_in[1]) - np.float32(drange_in[0]))
+        bias = (np.float32(drange_out[0]) - np.float32(drange_in[0]) * scale)
+        data = data * scale + bias
+    return th.clamp(data, min=0, max=1)
+
+
+def get_image(gen, point, depth, alpha):
+    image = gen(point, depth, alpha).detach()
+    image = adjust_dynamic_range(image).squeeze(dim=0)
+    return image.cpu().numpy().transpose(1, 2, 0)
+
+
+def main(args):
+    """
+    Main function of the script
+    :param args: parsed commandline arguments
+    :return: None
+    """
+    from pro_gan_pytorch.PRO_GAN import Generator
+
+    # create generator object:
+    print("Creating a generator object ...")
+    generator = th.nn.DataParallel(
+        Generator(depth=args.depth,
+                  latent_size=args.latent_size).to(device))
+
+    # load the trained generator weights
+    print("loading the trained generator weights ...")
+    generator.load_state_dict(th.load(args.generator_file))
+
+    # total_frames in the video:
+    total_frames = int(args.time * args.fps)
+
+    # Let's create the animation video from the latent space interpolation
+    # all latent vectors:
+    all_latents = th.randn(total_frames, args.latent_size).to(device)
+    all_latents = gaussian_filter(all_latents.cpu(), [args.smoothing * args.fps, 0])
+    all_latents = th.from_numpy(all_latents)
+    all_latents = all_latents / all_latents.norm(dim=-1, keepdim=True) \
+                  * (sqrt(args.latent_size))
+
+    # create output directory
+    os.makedirs(args.out_dir, exist_ok=True)
+
+    global_frame_counter = 1
+    # Run the main loop for the interpolation:
+    print("Generating the video frames ...")
+    for latent in tqdm(all_latents):
+        latent = th.unsqueeze(latent, dim=0)
+
+        # generate the image for this point:
+        img = get_image(generator, latent, args.depth - 1, 1)
+
+        # save the image:
+        plt.imsave(os.path.join(args.out_dir, str(global_frame_counter) + ".png"), img)
+
+        # increment the counter:
+        global_frame_counter += 1
+
+    # video frames have been generated
+    print("Video frames have been generated at:", args.out_dir)
+
+
+if __name__ == "__main__":
+    main(parse_arguments())
