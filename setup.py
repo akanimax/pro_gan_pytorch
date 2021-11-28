dcc5718604c3da1244ff4d17343b69be96124127
akanimax <animeshsk3@gmail.com>
2021-11-28 17:36:52 +0000

    pip install pro-gan-pth works now :)

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
-from setuptools import find_packages, setup
-
-with open("README.md", "r") as file_:
-    project_description = file_.read()
+from setuptools import setup
 
 with open("requirements.txt", "r") as file_:
     project_requirements = file_.read().split("\n")
 
 setup(
     name="pro-gan-pth",
-    version="3.3",
-    packages=find_packages(".", exclude=("test", "samples")),
+    version="3.3333",
+    packages=["pro_gan_pytorch", "pro_gan_pytorch_scripts"],
     url="https://github.com/akanimax/pro_gan_pytorch",
     license="MIT",
     author="akanimax",
     author_email="akanimax@gmail.com",
+    setup_requires=['wheel'],
     description="ProGAN package implemented as an extension of PyTorch nn.Module",
-    long_description=project_description,
     install_requires=project_requirements,
     entry_points={
         "console_scripts": [
-            f"progan_train=scripts.train:main",
-            f"progan_lsid=scripts.latent_space_interpolation:main",
-            f"progan_fid=scripts.compute_fid:main",
+            f"progan_train=pro_gan_pytorch_scripts.train:main",
+            f"progan_lsid=pro_gan_pytorch_scripts.latent_space_interpolation:main",
+            f"progan_fid=pro_gan_pytorch_scripts.compute_fid:main",
         ]
     },
 )
